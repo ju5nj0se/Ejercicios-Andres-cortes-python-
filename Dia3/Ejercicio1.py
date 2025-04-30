aac85704c57a45c88175d0133c64c58cd03ab62d
ju5nj0se <juaan.josehernandez@gmail.com>
2025-04-29 21:35:51 -0500

    Update Ejercicio1.py
    Ejercicio con la def crear

--- Dia3/Ejercicio1.py
+++ Dia3/Ejercicio1.py
@@ -11,39 +11,35 @@ print("---------------------------------------------\n" \
 
 #Creacion del diccionario Biblioteca
 Biblioteca = {
-    1: {'titulo': 'Cien años de soledad', 'autor': 'Gabriel García Márquez', 'año': 1967},
-    2: {'titulo': '1984', 'autor': 'George Orwell', 'año': 1949},
-    3: {'titulo': 'Noches Blancas', 'autor': 'Fyodor Dostoyevski', 'año': 1917}
+    0: {'titulo': 'Cien años de soledad', 'autor': 'Gabriel García Márquez', 'año': 1967},
+    1: {'titulo': '1984', 'autor': 'George Orwell', 'año': 1949},
+    2: {'titulo': 'Noches Blancas', 'autor': 'Fyodor Dostoyevski', 'año': 1917}
 }
 
 #Variables que van a funcionar como filtros en caso de que el valor se erroneo o directamente no se introduzca un valor
-ID = 0
-Titulo = ""
-Autor = ""
-Año = None
+ID = int()
+Titulo = str()
+Autor = str()
+Año = int()
 
 
 # Creacion de la funciones que nos van a permitir hacer las diferentes acciones
-def Crear(ID, Titulo, Autor, Año): #Agregar nuevos libros al diccionario
-    ID = Biblioteca.key[-1]+1
+def Crear(Titulo, Autor, Año): #Agregar nuevos libros al diccionario
+    ID = int(len(Biblioteca))
+    print(Biblioteca)
+    Biblioteca.setdefault(ID, (Titulo, Autor, Año))
+    print(Biblioteca)
+
+def Buscar(ID, Titulo): #Mostrar todos lo libros almacenados y permitir buscar un libro por su ID o Titulo
+    if ID != None:
+        if ID in Biblioteca.keys():
+            print(f"Id: {Biblioteca.get(ID)}")
     
-
-'''
-def Leer(): #Muestro todos los libros almacenados y permite buscar libros por su ID O Titulo
-
-def Actualizar(): #Modifica la informacion de un libro por su ID 
-
-def Eliminar(): #Elimina un libro por su ID
-
-'''
-
-Op = input("Elija una opcion")
+Op = int(input("Elija una opcion: "))
 
 
 if Op == 1:
-    ID = int(input("Ingrese un id para el libro: "))
     Titulo = input("Ingrese un titulo para el libro: ")
     Autor = input("Ingrese un autor para el libro: ")
     Año = int(input("Ingrese el año de publicacion del libro: "))
-    Crear(ID, Titulo, Autor, Año)
-elif Op == 2:
+    Crear(Titulo, Autor, Año)
